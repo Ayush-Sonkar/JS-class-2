6baa006fa6a20daa02bd1172b6b732e6a30acc35
AyushSonkar1 <18381037-AyushSonkar1@users.noreply.replit.com>
2024-01-11 07:38:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Ayush developer");
+// Objects in JS - collectionof a key value pair
+
+let myrect = {
+  length: 4,
+  breadth: 5,
+
+	area: function() {
+		console.log("area is 20");}
+};
+
+console.log(myrect.area());
