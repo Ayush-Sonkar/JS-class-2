901ec4d8d0ead61530e9efe0f2b90dec48b3550b
Anshuking <103761071+Anshuking@users.noreply.github.com>
2024-01-11 08:46:14 +0000

    added about factory function

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 console.log("Ayush developer");
-// Objects in JS - collectionof a key value pair
+// Objects in JS - collection of a key value pair
 
 let myrect = {
   length: 4,
   breadth: 5,
 
-	area: function() {
-		console.log("area is 20");}
+  area: function () {
+    console.log("area is 20");
+  },
 };
 
 console.log(myrect.area());
+
+// Object creating methods in JS
+//factory function-used to create a single function and then claa t for using anytime
+//
+
+function createRectangle(len, bre) {
+  len;
+  bre;
+  return len, bre;
+}
+console.log(createRectangle(4, 5));
+
+function MRectangle(len, bre) {
+  return (rectangle = {
+    lenght: len,
+    breadth: bre,
+
+    draw() {
+      console.log("drawing rectangle");
+    },
+  });
+}
+let mrect = MRectangle(8, 5);
+console.log(mrect);
