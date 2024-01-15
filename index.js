ae8b87b754a8c62106bc4cc984d33ef4fa7d46b2
Anshuking <103761071+Anshuking@users.noreply.github.com>
2024-01-15 07:44:41 +0000

    Added Reference type

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let myrect = {
 console.log(myrect.area());
 
 // Object creating methods in JS
-//factory function-used to create a single function and then claa t for using anytime
+//factory function-used to create a single function and then call it for using anytime
 //
 
 function createRectangle(len, bre) {
@@ -36,21 +36,54 @@ function MRectangle(len, bre) {
 let mrect = MRectangle(8, 5);
 console.log(mrect);
 
-
 // constructor function
 function Rectangle(len, bre) {
-	this.lambai = len;
-	this.chaudhai= bre;
-	this.draw=function() {
-		console.log("Lets draw rectangle");
-	}
-	this.area=function(len,bre) {
-		return len*bre;
-	};
+  this.lambai = len;
+  this.chaudhai = bre;
+  this.draw = function () {
+    console.log("Lets draw rectangle");
+  };
+  this.area = function (len, bre) {
+    return len * bre;
+  };
 }
 
-let rec= new Rectangle(4,5);
+let rec = new Rectangle(4, 5);
 console.log(rec);
-console.log(rec.area(90,5));
+console.log(rec.area(90, 5));
+// adding and deleting in object
 
-	 
+rec.color = "red";
+delete rec.color;
+console.log(rec);
+
+// Reference type
+// array, object, function
+
+let a = 190;
+a++;
+let b = a;
+console.log(b);
+
+let c = { value: 10 };
+let d = c;
+c++;
+console.log(d);
+
+let z = 55;
+function incr(z) {
+  z++;
+}
+incr(z);
+
+
+console.log(incr(z));
+
+
+let x ={value: 50};
+function inc(x) {
+	x.value++;
+}
+inc(x);
+
+console.log(x.value);
